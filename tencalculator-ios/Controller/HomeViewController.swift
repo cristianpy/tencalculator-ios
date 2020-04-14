fe9b8745eec7b987dbe6a907a772212c9f1d69ef
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2020-04-14 17:22:52 -0400

    Added basic structure

---
+++ tencalculator-ios/Controller/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  tencalculator-ios
+//
+//  Created by Cristian Paniagua on 04/14/20.
+//  Copyright © 2020 Cristian Paniagua. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+  
+  // MARK: - Initialization
+  
+  init() {
+      super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+
+}
